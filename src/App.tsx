761c69d2569db8a4238a076171c5c3d174a42469
Yash Raj <yashr3037@gmail.com>
2023-02-23 10:07:59 +0530

    simple counter in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState<number>(0);
+  const handleIncrese = (): void => {
+    setCount(count + 1);
+  };
+  const handleDecrese = (): void => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <div className="App">
-      Hello typescript
+      <h1>Hello Mr Yash</h1>
+      <button onClick={handleIncrese}>Increment</button>
+      <p>{count}</p>
+      <button onClick={handleDecrese}>Decrement</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
